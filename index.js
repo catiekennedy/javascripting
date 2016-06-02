7cae183c3d8ab9040b571630ed9da8407bb15a80
Kennedy, Catie <ckennedy@vermeer.com>
2016-06-02 16:00:07 -0500

    in the process of making a triangle of starts

--- index.js
+++ index.js
@@ -4,18 +4,27 @@
 // Eloquent Javascript
 
 // Chapter 2
-var debt = 140;
-debt = debt -35;
-console.log(debt);
-console.log("");
-var num = 0;
-while (num <= 12) {
-    console.log(num);
-    num = num + 2;
-};
+// var debt = 140;
+// debt = debt -35;
+// console.log(debt);
+// console.log("");
+// var num = 0;
+// while (num <= 12) {
+//     console.log(num);
+//     num = num + 2;
+// };
 // practiced a do / while loop in chrome console
 // do {
 //     var yourName = prompt("who are you");
 // } while (!yourName);
 // console.log(yourName);
 // catie
+
+// triangle
+
+i = 0
+while (i < 7) {
+    console.log("*" * i);
+    i++;
+}
+
