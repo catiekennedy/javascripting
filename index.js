773ee286b0f8a8345c572c087dd11d054893049f
Kennedy, Catie <ckennedy@vermeer.com>
2016-06-03 10:21:03 -0500

    refactored fizzbuzz

--- index.js
+++ index.js
@@ -28,14 +28,36 @@
 // }
 
 // fizzbuzz
-for (i = 1; i <= 16; i++) {
-    if (i % 5 == 0 && i % 3 == 0) {
-        console.log("FizzBuzz");
-    } else if (i % 5 == 0) {
-        console.log("Buzz");
-    } else if (i % 3 == 0) {
-        console.log("Fizz");
-    } else {
-        console.log(i);
+// for (i = 1; i <= 16; i++) {
+//     if (i % 5 == 0 && i % 3 == 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// refactored fizzbuzz
+for (var i = 1; i <= 100; i++) {
+    var output = "";
+    if (i % 3 == 0) {
+        output += "Fizz";
     }
+    if (i % 5 == 0) {
+        output += "Buzz"
+    }
+    console.log(output || i);
 }
+
+
+
+
+
+
+
+
+
+
